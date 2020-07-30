a1931e31533a2c6168c06f4c31f69ae1b3039b9f
tune <hisatsune01@gmail.com>
2020-07-30 15:58:31 +0900

    study Alias

---
+++ practice/Alias.ts
@@ -0,0 +1,9 @@
+type Name = string;
+let firstname: Name = '大樹';
+let lastname: Name = '久恒';
+
+type Password = number | string;
+let password: Password = 'hisatune0423';
+
+type User = { id: number; name: string };
+const user: User = { id: 1, name: 'tune' };
